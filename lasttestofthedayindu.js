1141ad56310afda0be24b48c02bce15dea9997a2
Indunil1992 <indunil@adroitlogic.com>
2020-04-01 19:28:52 +0530

    k

--- lasttestofthedayindu.js
+++ lasttestofthedayindu.js
@@ -1,4 +1,16 @@
+let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
 exports.handler = async (event) => {
-    
-    return {"message": "Successfully executed-cognito"};
+    try {
+        let data = await s3.listObjects({
+            Bucket: "as2-test-lahiru",
+            MaxKeys: 10
+        }).promise();
+
+    } catch (err) {
+        // error handling goes here
+    };
+
+    return { "message": "Successfully executed-cognito" };
 };
